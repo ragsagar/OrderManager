39a2fad6dc81675cab31c9e7e80890eb970bbfa1
ragsagar-mq <ragsagar@mquotient.net>
2012-12-20 17:37:16 +0530

    Adding needed views and urls

--- food/views.py
+++ food/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+import json
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Food
+
+
+def index(request):
+    """
+    Show the index page.
+    """
+    return render(request, 'index.html')
+
+
+def test_api(request):
+    """
+    Serves test api.
+    """
+    values = json.dumps(list(Food.objects.values()))
+    return HttpResponse(values, content_type='application/json')
