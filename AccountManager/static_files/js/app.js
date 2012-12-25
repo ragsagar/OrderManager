29d75e542a861656b709a3378156329e49869d17
ragsagar-mq <ragsagar@mquotient.net>
2012-12-25 10:25:01 +0530

    Changes in backbone js file

--- AccountManager/static_files/js/app.js
+++ AccountManager/static_files/js/app.js
@@ -1,8 +1,15 @@
-window.Food = Backbone.Model.extend();
+window.Food = Backbone.Model.extend({
+        urlRoot: "/food/api",
+        defaults: {
+            "id": null,
+            "name": "",
+            "price": ""
+        }
+});
 
 window.FoodCollection = Backbone.Collection.extend({
         model: Food,
-        url: "/food/test_api/"
+        url: "/food/api"
 });
 
 window.FoodListView = Backbone.View.extend({
@@ -10,6 +17,10 @@ window.FoodListView = Backbone.View.extend({
         
         initialize: function() {
             this.model.bind("reset", this.render, this);
+            var self = this;
+            this.model.bind("add", function(food) {
+                $(self.el).append(new FoodListItemView({model: food}).render().el);
+            });
         },
         
         render: function(eventName){
@@ -25,25 +36,112 @@ window.FoodListItemView = Backbone.View.extend({
     tagName: "li",
     
     template: _.template($('#food-item').html()),
+
+    initialize: function() {
+        this.model.bind("change", this.render, this);
+        this.model.bind("destroy", this.close, this);
+    },
     
     render: function(evenName) {
         $(this.el).html(this.template(this.model.toJSON()));
         return this;
+    },
+    
+    close: function() {
+        $(this.el).unbind();
+        $(this.el).empty();
     }
     });
 
 window.FoodView = Backbone.View.extend({
     template: _.template($('#food-item-detail').html()),
+
+    initialize: function() {
+        this.model.bind("change", this.render, this);
+    },
+
     render: function(eventName){
         $(this.el).html(this.template(this.model.toJSON()));
         return this;
+    },
+
+    events: {
+        "change input": "change",
+        "click .save": "saveFood",
+        "click .delete": "deleteFood"
+    },
+
+    change: function(event) {
+        var target = event.target;
+        //console.log("Changing " + target.id + " from " + target.defaultValue + ' to ' + target.value);
+        var change = {};
+        change[target.name] = target.value;
+        this.model.set(change);
+        //this.model.save();
+    },
+
+    saveFood: function(){
+        this.model.set({
+            name: $('#name').val(),
+            price: $('#price').val()
+        });
+        if (this.model.isNew()){
+            app.foodList.create(this.model);
+        } else {
+            this.model.save();
+        }
+        return false;
+    },
+
+    deleteFood: function(){
+        this.model.destroy({
+            success: function(){
+                alert('Food item deleted');
+                window.history.back();
+            }
+        });
+        return false;
+    },
+
+    close: function() {
+        $(this.el).unbind();
+        $(this.el).empty();
     }
-})
+});
+
+window.HeaderView = Backbone.View.extend({
+
+    template: _.template($('#food-header').html()),
+
+    initialize: function() {
+        this.render();
+    },
+
+    render: function(eventName) {
+        $(this.el).html(this.template());
+        return this
+    },
+    
+    events: {
+        "click .new": "newFood"
+    },
+
+    newFood: function(event) {
+        if (app.foodView) app.foodView.close();
+        app.foodView = new FoodView({model: new Food()});
+        $('#content').html(app.foodView.render().el);
+        return false;
+    }
+});
 
 var AppRouter = Backbone.Router.extend({
     routes:{
         "": "list",
-        "food/:id": "foodDetails"
+        ":id": "foodDetails"
+    },
+
+    initialize: function(){
+        $('#header').html(new HeaderView().render().el)
     },
 
     list:function(){
@@ -54,6 +152,7 @@ var AppRouter = Backbone.Router.extend({
     },
 
     foodDetails: function(id) {
+        if(!this.foodList) this.list()
         this.food = this.foodList.get(id);
         this.foodView = new FoodView({model: this.food});
         $('#content').html(this.foodView.render().el);
